507add0b71b3f1e050ef2fab3497e25ff392a978
mecarito <pieritomecar@gmail.com>
2021-05-22 19:03:53 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo " alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <input type="text" className="focus:bg-blue-500 p-1 rounded-md" />
       </header>
     </div>
   );

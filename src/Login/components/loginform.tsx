d9aca7750094b53a0907dc2562f8d049411fc46d
mecarito <pieritomecar@gmail.com>
2021-06-05 21:32:05 +0300

    create responsive mobile design for the login page

--- src/Login/components/loginform.tsx
+++ src/Login/components/loginform.tsx
@@ -5,25 +5,25 @@ import Flash from "../../assets/flash.svg";
 export default function Loginform() {
   return (
     <>
-      <div className="flex flex-col gap-4 pt-8 px-20">
+      <div className="flex flex-col gap-4 pt-8 px-10 items-center">
         <img src={Flash} alt="flash" className="w-8 h-8" />
         <h1 className="text-2xl text-black font-bold"> Login</h1>
-        <h6 className="text-gray-600 text-sm">
+        <h6 className="text-gray-600 text-sm text-center">
           See your growth and get consulting support!
         </h6>
         <button
           className="flex flex-row rounded-full py-2 justify-center gap-4 items-center
-         ring-1 ring-gray-300 w-80 focus:ring-indigo-500 focus:outline-none mt-6"
+         ring-1 ring-gray-300 w-60 focus:ring-indigo-500 focus:outline-none mt-4"
         >
           <img src={Googlelogo} alt="google logo" className="w-6 h-6" />
           <p className="text-black text-sm"> Sign in with Google</p>
         </button>
-        <div className="flex items-center space-x-2 mt-2 mb-1">
-          <div className="w-24 h-[1px] bg-gray-300"></div>
+        <div className="flex space-x-2 mt-2 mb-1 mx-auto items-center">
+          <div className="w-16 h-[1px] bg-gray-300"></div>
           <p className="text-xs text-gray-500 text-opacity-80">
             or Sign in with Email
           </p>
-          <div className="w-24 h-[1px] bg-gray-300"></div>
+          <div className="w-16 h-[1px] bg-gray-300"></div>
         </div>
         <div>
           <label
@@ -38,7 +38,7 @@ export default function Loginform() {
             id="email"
             placeholder="mail@website.com"
             autoComplete="email"
-            className=" w-80 mt-1 focus:ring-indigo-500 focus:border-indigo-500 
+            className=" w-70 mt-1 focus:ring-indigo-500 focus:border-indigo-500 
           shadow-sm sm:text-sm border-gray-300 rounded-full
           placeholder-gray-500 placeholder-opacity-80"
           />
@@ -56,29 +56,29 @@ export default function Loginform() {
             placeholder="Min. 8 characters"
             id="password"
             autoComplete="password"
-            className="w-80 mt-1 focus:ring-indigo-500 focus:border-indigo-500 
+            className="w-70 mt-1 focus:ring-indigo-500 focus:border-indigo-500 
            shadow-sm sm:text-sm border-gray-300 rounded-full
            placeholder-gray-500 placeholder-opacity-80"
           />
         </div>
-        <div className="flex flex-row items-center">
+        <div className="flex gap-2 items-center">
           <input
             className="appearance-none rounded checked:bg-indigo-600 checked:border-transparent focus:ring-0"
             type="checkbox"
           />
-          <p className="ml-2 text-black text-sm">Remember me</p>
-          <p className="ml-20 text-sm text-indigo-900 cursor-pointer">
-            Forgot password?
-          </p>
+          <p className=" text-black text-sm">Remember me</p>
         </div>
 
         <button
-          className="flex flex-row rounded-full py-2 justify-center gap-4 items-center
-         ring-1 ring-gray-300 w-80 focus:ring-purple-500 focus:outline-none mt-2
+          className="flex flex-row rounded-full px-16 py-2 justify-center gap-4 items-center
+         ring-1 ring-gray-300 w-70 focus:ring-purple-500 focus:outline-none mt-2
          bg-indigo-600"
         >
           <p className="text-white text-sm"> Login </p>
         </button>
+        <p className=" text-sm text-indigo-900 cursor-pointer text-center">
+          Forgot password?
+        </p>
         <p className="text-sm mt-1 mb-4">
           Not registered yet?
           <span className="ml-2 text-indigo-900 cursor-pointer">
--- src/Login/pages/loginpage.tsx
+++ src/Login/pages/loginpage.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import Loginform from "../components/loginform";
-import Loginnote from "../components/loginnote";
+// import Loginnote from "../components/loginnote";
 
 export default function Loginpage() {
   return (
     <>
-      <div className="px-20 py-10 bg-gray-300 ">
+      <div className="">
+        <div className="">
+          <div className="">
+            <Loginform />
+          </div>
+          {/* <div className="py-40 px-20  bg-indigo-600 rounded-r-lg ">
+            <Loginnote />
+          </div> */}
+        </div>
+      </div>
+
+      {/* <div className="px-20 py-10 bg-gray-300 ">
         <div className="grid grid-cols-2 bg-white rounded-lg">
           <div className="px-10 ">
             <Loginform />
           </div>
-          <div className="py-40 px-20  bg-indigo-600 rounded-r-lg">
+          <div className="py-40 px-20  bg-indigo-600 rounded-r-lg ">
             <Loginnote />
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }

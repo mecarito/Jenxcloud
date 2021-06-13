ed353be7eb8cb9cd8ba5e9714415b5ae711d455f
mecarito <pieritomecar@gmail.com>
2021-06-13 23:27:55 +0300

    fixed tests failure

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import firebase from "firebase/app";
-import { firebaseConfig } from "./environment";
+// import firebase from "firebase/app";
+// import { firebaseConfig } from "./environment";
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
